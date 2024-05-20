a2a985f8bab4afe872ff8394759b8d1b32ec156f
ZyWenn31 <144626089+ZyWenn31@users.noreply.github.com>
2024-05-20 15:58:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    mousewheel: true,
+    centeredSlide:true,
+    slidesPerView: 4,
+    spaceBetween: 1,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
